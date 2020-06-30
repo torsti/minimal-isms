a7619005193f983a8e22f58ff5488349810daf1c
Torsti Schulz <torsti.schulz@iki.fi>
2020-06-30 10:48:04 +0300

    Add simple variance calculation

--- composition.R
+++ composition.R
@@ -11,9 +11,26 @@ P <- function(f, args) {
 }
 
 integral <- function(f, a = -Inf, b = Inf, subdivisions = 1000, rel.tol = .Machine$double.eps^0.5, abs.tol = rel.tol) {
-    integrate(f, a, b, subdivisions = subdivisions, rel.tol = rel.tol, abs.tol = abs.tol)
+    y <- (integrate(f, a, b, subdivisions = subdivisions, rel.tol = rel.tol, abs.tol = abs.tol))
+    v <- y$value
+    attr(v, "integration") <- y
+
+    return(v)
 }
 
 E <- function(p, a = -Inf, b = Inf, subdivisions = 1000, rel.tol = .Machine$double.eps^0.5, abs.tol = rel.tol) {
-    integrate(function(x) { x * p(x) }, a, b, subdivisions = subdivisions, rel.tol = rel.tol, abs.tol = abs.tol)
+    y <- (integrate(function(x) { x * p(x) }, a, b, subdivisions = subdivisions, rel.tol = rel.tol, abs.tol = abs.tol))
+    v <- y$value
+    attr(v, "integration") <- y
+
+    return(v)
+}
+
+Var <- function(p, a = -Inf, b = Inf, subdivisions = 10000, rel.tol = .Machine$double.eps^0.5, abs.tol = rel.tol) {
+    mu <- (integrate(function(x) { x * p(x) }, a, b, subdivisions = subdivisions, rel.tol = rel.tol, abs.tol = abs.tol))$value
+    y <- (integrate(function(x) { (x^2 -2 * x * mu + mu^2) * p(x) }, a, b, subdivisions = subdivisions, rel.tol = rel.tol, abs.tol = abs.tol))
+    v <- y$value
+    attr(v, "integration") <- y
+
+    return(v)
 }
